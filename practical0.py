1914aa6d74b671379c100904693423d5dad5431c
maytow96 <02240135.cst@rub.edu.bt>
2025-03-11 22:15:16 +0600

    My first commit

---
+++ practical0.py
@@ -0,0 +1,4 @@
+studentName = "Tshering lhamo"
+studentNumber = "02240135"
+
+print("My name is " + studentName + " and my student number is " + studentNumber)
--- practical0.py/Untitled-1.py
+++
@@ -1,4 +0,0 @@
-studentName = "tshering lhamo"
-studentNumber = "02240134"
-
-print("My name is " + studentName + " and my student number is " + studentNumber)
